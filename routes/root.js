428971909a5f783a3a05242ca2606aef43d5e5fa
Daniel Koch <daniel@suitsoft.eu>
2017-06-12 06:45:23 +0200

    Add a dummy route

---
+++ routes/root.js
@@ -0,0 +1,14 @@
+/**
+ * Root route
+ */
+const pkg = require('../package.json');
+
+const getRoot = function(req, res) {
+    res.json({
+        version: pkg.version
+    });
+};
+
+module.exports = function(server) {
+    server.get('/', getRoot);
+};
